c396b26069399da053d2dc77439627ff8ff921d3
chayadevi1310 <chayadevi1310@gmail.com>
2024-09-13 20:46:44 +0530

    Add files via upload

---
+++ src/Sept/ex_13092024/Task2.java
@@ -0,0 +1,21 @@
+package Sept.ex_13092024;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    double a = 34;
+    double b = 10;
+    System.out.printf("%f * %f = %f", a,b,a*b);
+    System.out.println();
+    System.out.printf("%f + %f = %f", a, b, a+b);
+    System.out.println();
+    System.out.printf("%f - %f = %f", a,b,a-b);
+    System.out.println();
+    System.out.printf("%f / %f = %f", a, b, a/b);
+    System.out.println();
+    System.out.printf("%f modulus %f = %f", a, b, a%b);
+
+}
+
+
+}
