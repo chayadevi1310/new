12eb3856844fe6c46982630c000803fd4535616c
chayadevi1310 <chayadevi1310@gmail.com>
2024-09-13 19:18:21 +0530

    Initial commit

---
+++ src/Sept/ex_02092024/Lab001.java
@@ -0,0 +1,4 @@
+package Sept.ex_02092024;
+
+public class Lab001 {
+}
---
+++ src/Sept/ex_13092024/Lab019.java
@@ -0,0 +1,17 @@
+package Sept.ex_13092024;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+
+        int n=10;
+        int i;
+        for(i=0;i<n;i++)
+        {
+            System.out.printf("n*(i+1) = ");
+        }
+
+            }
+
+}
+
