baa7f42627489c328807a6b938f2a74bf674ef58
chayadevi1310 <chayadevi1310@gmail.com>
2024-09-23 18:13:46 +0530

    Merge remote-tracking branch 'origin/sept' into sept

---
+++ src/Sept/ex_23092024/Lab001.java
@@ -0,0 +1,20 @@
+package Sept.ex_23092024;
+
+public class Lab001 {
+public static void main(String[] args){
+    /*Create a simple calculator that performs addition, subtraction, multiplication, and division, modus based on user input using switch statements.
+
+Inputs :   num 1, num 2, +
+Output :  num1+num2 → print information.
+
+Write a program that takes an integer input (1-12) and prints the name of the corresponding month using a switch statement.
+Input  - 12
+Out - Dec.
+
+Write a program that converts between different units (e.g., kilometers to miles, Celsius to Fahrenheit) based on user selection using a switch statement.
+Input. -
+choice - 1 - km → m, km → 1km
+choice - 2 - f → c, f → c*/
+}
+
+}
